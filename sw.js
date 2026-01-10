dc65ee9f161508c49fb42acc940eb807d901032b
lennymos123 <paulmoseley12345@gmail.com>
2026-01-10 05:37:06 +0000

    Update cache name to darts-trainer-v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "darts-trainer-v1";
+const CACHE_NAME = "darts-trainer-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
